40a067e49b221cb7019face9c7e74b2496231499
Karlo <100581912+kkranjcin20@users.noreply.github.com>
2024-01-28 19:39:38 +0100

    Update PieceType.py

--- chess/objects/PieceType.py
+++ chess/objects/PieceType.py
@@ -4,14 +4,4 @@ class PieceType(Persistent):
     def __init__(self, typeid, typename):
         self.typeid = typeid
         self.typename = typename
-
-"""
-ubaci ovih 6 tipova rucno u bazu
-
-1-King
-2-Queen
-3-Rook
-4-Knight
-5-Bishop
-6-Pawn
-"""
+        
